fba040a58416cdc4681a2d0ada706232f389940e
zachnewburgh <zach.newburgh@gmail.com>
2016-03-21 13:29:15 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
 
+  def song_artist_id(song)
+    if song.artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    else
+      f.hidden_field "song[artist_id]", song.artist_id
+    end
+  end
+
 end
